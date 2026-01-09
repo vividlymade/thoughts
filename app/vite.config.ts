892346cddb270395f6d6f836496bbcf885cb4f0a
vividlymade <104797750+vividlymade@users.noreply.github.com>
2026-01-09 19:46:20 +0000

    Remove old commented out configuration parts from `vite.config.ts`

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,16 +1,7 @@
 import { defineConfig } from 'vitest/config'
 import tailwindcss from '@tailwindcss/vite'
 import arraybuffer from 'vite-plugin-arraybuffer'
-// import { svelte } from '@sveltejs/vite-plugin-svelte'
-
 import { sveltekit } from '@sveltejs/kit/vite'
-// import { esbuildDecorators } from '@anatine/esbuild-decorators'
-// import swc from '@rollup/plugin-swc'
-
-// export default defineConfig({
-//   plugins: [sveltekit()]
-// });
-
 
 export default defineConfig({
   build: {
@@ -18,35 +9,18 @@ export default defineConfig({
     /** Necessary to keep the builds clean. */
     emptyOutDir: true,
   },
-  // root: 'src',
-  // base: './',
-  // preview: {
-  //   host: true,
-  //   port: 80,
-  //   strictPort: true
-  // },
-  // server: {
-  //   host: true,
-  //   port: 80,
-  //   strictPort: true
-  // },
   envDir: '../',
   ssr: {
     external: ['reflect-metadata', 'pg'],
     optimizeDeps: {
       esbuildOptions: {
-        plugins: [
-          // esbuildDecorators({
-          //   tsconfig: './tsconfig.json',
-          // }),
-        ],
+        plugins: [],
         minify: true,
       },
     },
   },
   plugins: [
     arraybuffer(),
-	// svelte()
     tailwindcss(),
     sveltekit(),
   ],
