eb071afd0d0ce3cd7356d9d7b1038a7cd8cfcba8
vividlymade <104797750+vividlymade@users.noreply.github.com>
2026-01-09 19:36:11 +0000

    Improve `settings/account` route styling

--- app/src/routes/(app)/settings/account/+page.svelte
+++ app/src/routes/(app)/settings/account/+page.svelte
@@ -8,7 +8,7 @@
     const localUserId = $derived(data.localUserId)!
 </script>
 <div class="flex-1 flex flex-col justify-between space-y-6">
-    <div class="text-xs font-extralight *:rounded-2xl *:p-2.5">
+    <div class="text-xs font-extralight *:rounded-2xl *:p-2.5 bg-white-50 border bg-white border-zinc-100 drop-shadow-sm/3 rounded-2xl overflow-clip">
         <ChangeEmailButton/>
         <ChangePasswordButton/>
     </div>
